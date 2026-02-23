43bf02ea85487c0b8e69a29d32780960696a4c4a
Muna Hamsho <rhymeaya@gmail.com>
2026-02-23 22:37:03 +0300

    Fix vercel folders

--- finbank-backend/server.js
+++ finbank-backend/server.js
@@ -42,6 +42,11 @@ app.use('/api/tickets', ticketRoutes);
 app.use('/api/reports', reportRoutes); // في الأسفل مع باقي المسارات
 app.use('/api/payments', paymentRoutes);
 app.use('/api/wallet', walletRoutes);
+app.use(cors({
+  origin: ["https://finbank-frontend.vercel.app"], // رابط الفرونت إند بعد النشر
+  credentials: true
+}));
+
 
 
 
@@ -79,3 +84,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
+// في نهاية الملف
+module.exports = app;
